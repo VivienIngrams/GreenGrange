d42005f9a650252ac39e1388068d6014969e7a25
Vivien <vivingrams@gmail.com>
2025-03-05 14:49:28 +0000

    Deploy

--- nextjs-app/app/components/Header.tsx
+++ nextjs-app/app/components/Header.tsx
@@ -2,7 +2,19 @@ export default function Header() {
   return (
     <header className="bg-primary text-primary-foreground">
       <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold">Menu?</h1>
+       <nav>
+          <ul className="flex justify-between items-center py-4">
+            <li>
+              <a href="/" className="text-lg font-bold">Next.js App</a>
+            </li>
+            <li>
+              <a href="/activites" className="text-lg">Activities</a>
+            </li>
+            <li>
+              <a href="/house" className="text-lg">House</a>
+            </li>
+          </ul>
+       </nav>
       </div>
     </header>
   )
