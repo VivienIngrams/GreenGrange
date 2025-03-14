d9944593d8bf617bef58dfd1213e3112ab592ea7
Vivien <vivingrams@gmail.com>
2025-03-14 17:54:09 +0000

    Link to studio

--- nextjs-app/app/components/Footer.tsx
+++ nextjs-app/app/components/Footer.tsx
@@ -1,6 +1,8 @@
+import { LockKeyhole } from "lucide-react"
+
 export default function Footer() {
   return (
-    <footer className="bg-muted py-4 mt-8">
+    <footer className="relative bg-muted py-4 mt-8">
       <div className="mx-auto px-4 text-center">
         <p className="text-muted-foreground">
           For booking inquiries, please contact us{" "}
@@ -8,6 +10,13 @@ export default function Footer() {
             
           </a>
         </p>
+      </div>
+      <div>
+        <a href="https://green-grange-studio.vercel.app/structure"
+        target="_blank" rel="noopener noreferrer" className="absolute bottom-4 right-4 text-[#d3e6c4] ">
+          <LockKeyhole size={24} />
+        </a>
+      
       </div>
     </footer>
   )
